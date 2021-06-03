dfb75828f9d893440c598b44c83281bd26f0ac89
krzwier <krzwier@gmail.com>
2021-06-03 10:44:20 -0500

    Makes filter checkboxes functional

--- src/Hotels/index.js
+++ src/Hotels/index.js
@@ -1,27 +1,21 @@
 import React from "react";
 import Hotel from "./Hotel.js";
+import { hotels } from "../data.js";
 
-/* callback function
- - renders a single hotel
- - assigns a unique key prop to each Hotel component
- - passes the hotel argument to each Hotel component as a prop called "hotel"
- */
-const renderHotel = (hotel) => 
-    <Hotel key={hotel.id} hotel={hotel} />;
-
-/* functional component
-    - accepts 'Hotels' data model as a prop
-    - maps over the array of hotel data, calling the
-      'renderHotel' callback function to render a 'Hotel'
-      component for each item in the array
-*/
+const renderHotel = (hotel) => <Hotel key={hotel.id} hotel={hotel} />;
 
 const HotelsList = (props) => {
-  if (!props.hotels) {
-    return null;
-  }
-  const hotelListElements = props.hotels.map(renderHotel);
+  /* see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter to learn how the .filter array method works
 
+  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every to learn how the .every array method works
+  */
+  const filterCallback = (hotel) => {
+    const everyCallback = (filter) => hotel[filter];
+    return props.selectedFilters.every(everyCallback);
+  };
+  const filteredHotels = hotels.filter(filterCallback);
+
+  const hotelListElements = filteredHotels.map(renderHotel);
   return <ul className="hotels-list">{hotelListElements}</ul>;
 };
 
